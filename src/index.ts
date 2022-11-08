2cee7714214b69c60dd93f1cd4479f8443b477f6
Ethan-Prieur1 <Ethan-Prieur1@users.noreply.github.com>
2022-11-08 14:57:00 +0000

    Prettified Code!

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,20 @@
  * Since:   2022-11-01
  */
 
-  function reverseString(string: string): string {
-        if (string === "") {
-        return "";
-        } else {
-        return reverseString(string.substr(1)) + string.charAt(0);
-        }
+function reverseString(string: string): string {
+  if (string === '') {
+    return ''
+  } else {
+    return reverseString(string.substr(1)) + string.charAt(0)
+  }
 }
 
-  function main() {
-        const aString = 'recursion'
-        
-        console.log('')
-        console.log(`the original string is: ${aString}`)
-        console.log(`the reversed string is: ${reverseString(aString)}`)
-        console.log('\nDone.')
+function main() {
+  const aString = 'recursion'
+
+  console.log('')
+  console.log(`the original string is: ${aString}`)
+  console.log(`the reversed string is: ${reverseString(aString)}`)
+  console.log('\nDone.')
 }
 main()
